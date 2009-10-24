fa2ea53bc04facf952cf5d0ee3e2bce1e1f5cd26
Marek Majkowski <majek@lshift.net>
2009-10-23 22:01:00 -0400

    x

---
+++ src/ydb_inline.h
@@ -0,0 +1,13 @@
+#define MOD_ADLER 65521
+static u32 adler32(void *sdata, size_t len) {
+	u8 *data = (u8*)sdata;
+	u32 a = 1, b = 0;
+	
+	while (len != 0) {
+		a = (a + *data++) % MOD_ADLER;
+		b = (b + a) % MOD_ADLER;
+		len--;
+	}
+	
+	return (b << 16) | a;
+}
